8563cbb0d0001e863a180bdabfebf5a7fd211274
MateMiklos <miklos.mate.91@gmail.com>
2019-08-02 06:57:48 +0200

    methods implemented, tests initialised

--- src/main/java/com/codecool/shop/dao/implementation/JDBC/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/JDBC/SupplierDaoJdbc.java
@@ -1,29 +1,44 @@
 package com.codecool.shop.dao.implementation.JDBC;
 
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Supplier;
 import com.codecool.shop.model.User;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.List;
 
 import static org.postgresql.jdbc2.EscapedFunctions.DATABASE;
 
-public class SupplierDaoJdbc {
+public class SupplierDaoJdbc implements SupplierDao {
     private static final String DATABASE = "jdbc:postgresql://localhost:5432/webshop";
     private static final String DB_USER = "mate";
     private static final String DB_PASSWORD = "123";
 
     @Override
-    public void add() {
-        String query = "INSERT INTO \"\" (name, email) VALUES ('" +  +
-                "','" +  + "');";
+    public void add(Supplier supplier) {
+        String query = "INSERT INTO supplier (name, description) VALUES ('" + supplier.getName()
+                + "','" + supplier.getDescription() + "');";
         executeQuery(query);
     }
 
     @Override
     public void remove(int id) {
-        String query = "DELETE FROM table WHERE " + id + " ";
+        String query = "DELETE FROM \"supplier\" WHERE id=" + id + ";";
+        executeQuery(query);
+    }
+
+    @Override
+    public List<Supplier> getAll() {
+        String query = "SELECT * FROM \"supplier\"";
+        executeQuery(query);
+        return null;
+    }
+
+    @Override
+    public Supplier find(int id) {
+        String query = "SELECT * FROM \"supplier\" WHERE id=" + id + ";";
+        executeQuery(query);
+        return null;
     }
 
     private Connection getConnection() throws SQLException {
@@ -35,7 +50,7 @@ public class SupplierDaoJdbc {
 
     private void executeQuery(String query) {
         try (Connection connection = getConnection();
-             Statement statement =connection.createStatement();
+             Statement statement =connection.createStatement()
         ){
             statement.execute(query);
 
